eb5823307efeeb32e001973453b6b94ee41978dc
Walter Yang <yandongy@codeaurora.org>
2015-04-09 15:00:11 +0800

    ASoC: wcd: avoid the fake button release of headset hook
    When we long press the headset hook key and then the system
    resumes from suspend mode, it will report the release event
    immediately from the input system level which we call a fake
    release. It will mislead the userspace layer to make the
    incorrect decision on the long or short press.
    So we set the INPUT_PROP_NO_DUMMY_RELEASE in property bit of the
    Button Jack to avoid this fake release.
    
    Change-Id: I485e01c0d1e10b41a203247b86e9fc5a2fa63179
    Signed-off-by: Walter Yang <yandongy@codeaurora.org>

--- sound/soc/codecs/wcd9xxx-mbhc.c
+++ sound/soc/codecs/wcd9xxx-mbhc.c
@@ -5648,6 +5648,9 @@ int wcd9xxx_mbhc_init(struct wcd9xxx_mbhc *mbhc, struct wcd9xxx_resmgr *resmgr,
 		}
 //weihung [NBQM-61]porting aduio function
 
+		set_bit(INPUT_PROP_NO_DUMMY_RELEASE,
+			mbhc->button_jack.jack->input_dev->propbit);
+
 		INIT_DELAYED_WORK(&mbhc->mbhc_firmware_dwork,
 				  wcd9xxx_mbhc_fw_read);
 		INIT_DELAYED_WORK(&mbhc->mbhc_btn_dwork, wcd9xxx_btn_lpress_fn);
