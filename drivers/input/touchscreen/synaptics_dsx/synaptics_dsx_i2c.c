b23312449c4fed22360d7000a22784160d3fa7bc
zhaoyuan <yzhao@codeaurora.org>
2015-12-21 11:14:05 +0800

    input: synaptics: defer touch resume to workqueue
    During device resume, the touch resume function is called after
    display resume. Touch resume function will take about >200ms.
    Defer the touch resume function to a workqueue to reduce the total
    device resume time. An optional DT property is added to enable this
    on targets that need this feature.
    
    Change-Id: Ia9b055144c5a7f29f0f0d57428cccbe15a7d7a87
    Signed-off-by: zhaoyuan <yzhao@codeaurora.org>

--- drivers/input/touchscreen/synaptics_dsx/synaptics_dsx_i2c.c
+++ drivers/input/touchscreen/synaptics_dsx/synaptics_dsx_i2c.c
@@ -1,7 +1,7 @@
 /*
  * Synaptics DSX touchscreen driver
  *
- * Copyright (c) 2014, The Linux Foundation.  All rights reserved.
+ * Copyright (c) 2014-2016, The Linux Foundation.  All rights reserved.
  *
  * Linux foundation chooses to take subject only to the GPLv2 license terms,
  * and distributes only under these terms.
@@ -435,6 +435,10 @@ static int synaptics_dsx_parse_dt(struct device *dev,
 		return rc;
 	}
 /* end  NBQ - EricHsieh - [06-601] */
+
+	rmi4_pdata->resume_in_workqueue = of_property_read_bool(np,
+			"synaptics,resume-in-workqueue");
+
 	rmi4_pdata->reset_delay_ms = RESET_DELAY;
 	rc = of_property_read_u32(np, "synaptics,reset-delay-ms", &temp_val);
 	if (!rc)
--- include/linux/input/synaptics_dsx_v2.h
+++ include/linux/input/synaptics_dsx_v2.h
@@ -5,6 +5,7 @@
  *
  * Copyright (C) 2012 Alexandra Chin <alexandra.chin@tw.synaptics.com>
  * Copyright (C) 2012 Scott Lin <scott.lin@tw.synaptics.com>
+ * Copyright (c) 2015-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -105,6 +106,7 @@ struct synaptics_dsx_board_data {
 	u32 disp_maxy;
 	bool disable_gpios;
 	bool detect_device;
+	bool resume_in_workqueue;
 	const char *fw_name;
 /* NBQ - EricHsieh - [06-23] - [Touch] Synaptics touch driver porting */
 	int rx_of_key;
